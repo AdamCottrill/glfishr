60ffb0f619eb767a8f47e5786234cff95daea67a
Rachel Henderson <rhenderson1993@gmail.com>
2023-01-19 13:22:47 -0500

    Changed get_species() function to get the species list from the Common app instead of FN_Portal. The FN_Portal species_list endpoint only provided 3 fields of data; the Common species endpoint provides 4 by default, and biological data (ranges of lengths and widths) when detail=TRUE is specified as a filter. Added function to provide the Common root for get_species() and other functions that access the Common portal. Ran devtools::document() to update the package structure.

--- R/get_species.R
+++ R/get_species.R
@@ -1,15 +1,15 @@
-#' Get Species list data from FN_Portal API
+#' Get Species list data from Common API
 #'
 #' This function accesses the api endpoint to for Species list
-#' available in Fn_portal. The species list does not contain all of the fields
-#' in common lookup table, but the fields are limited to spc,
-#' spc_nmco, and spc_nmsc.  This
-#' function takes an optional filter list which can be used to return
+#' available in Common. Only the fields spc, spc_nmco, spc_nmsc
+#' and species_at_risk are returned unless detail=TRUE is specified
+#' in the filter_list. The optional filter list can be used to return
 #' species based on part of their common or scientific name.
 #'
 #' See
 #' http://10.167.37.157/fn_portal/redoc/#operation/species_list_list
 #' for the full list of available filter keys (query parameters)
+#' 
 #'
 #' @param filter_list list
 #' @param to_upper - should the names of the dataframe be converted to
@@ -22,12 +22,13 @@
 #'
 #' species <- get_species()
 #' trout <- get_species(list(spc_nmco_like = "trout"))
+#' goby <- get_species(list(spc=366, detail=TRUE))
 get_species <- function(filter_list = list(), to_upper = TRUE) {
   query_string <- build_query_string(filter_list)
-  check_filters("species_list", filter_list)
+  check_filters("common_filters", filter_list)
   my_url <- sprintf(
-    "%s/species_list/%s",
-    get_fn_portal_root(),
+    "%s/species/%s",
+    get_common_portal_root(),
     query_string
   )
   payload <- api_to_dataframe(my_url)
