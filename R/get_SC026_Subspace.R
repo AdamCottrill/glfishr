0e0b17fed4d033a1c96e368670bb4b2e4b7ea9f6
Arthur Bonsall <120661682+ArthurBonsall@users.noreply.github.com>
2023-03-08 16:05:26 -0500

    Added the function get_SC016_Subspace

---
+++ R/get_SC026_Subspace.R
@@ -0,0 +1,41 @@
+#' Get SC026_Subspace - Spatial Strata Data Creel_Portal API
+#'
+#' This function accesses the api endpoint to for SC026_Subspace
+#' records. SC026_Subspace records contain information about the lower level
+#' spatial strata associated with a project.
+#'
+#' See
+#' http://10.167.37.157/creels/api/v1/redoc/#operation/sc026subspace_list
+#' for the full list of available filter keys (query parameters)
+#'
+#' @param filter_list list
+#' @param show_id When 'FALSE', the default, the 'id' and 'slug'
+#' fields are hidden from the data frame. To return these columns
+#' as part of the data frame, use 'show_id = TRUE'.
+#' @param to_upper - should the names of the dataframe be converted to
+#' upper case?
+#'
+#' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+#' @return dataframe
+#' @export
+#' @examples
+#'
+#' sc026_subspace <- get_SC026_Subspace(list(lake = "ON", year = 2012))
+#'
+#' sc026_subspace <- get_SC026_Subspace(list(prj_cd = "LHA_IA19_812"))
+#' sc026_subspace <- get_SC026_Subspace(list(prj_cd = "LHA_IA19_812"), show_id = TRUE)
+get_SC026_Subspace <- function(filter_list = list(), show_id = FALSE, to_upper = TRUE) {
+  recursive <- ifelse(length(filter_list) == 0, FALSE, TRUE)
+  query_string <- build_query_string(filter_list)
+  check_filters("sc026subspace", filter_list, api_app = "creels")
+  my_url <- sprintf(
+    "%s/sc026subspace/%s",
+    get_sc_portal_root(),
+    query_string
+  )
+  
+  payload <- api_to_dataframe(my_url, recursive = recursive)
+  payload <- prepare_payload(payload, show_id, to_upper)
+  
+  return(payload)
+}
