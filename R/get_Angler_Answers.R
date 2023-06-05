03064ee76fed2606d735c7cf3e3237b771a7f8cc
Rachel Henderson <rhenderson1993@gmail.com>
2023-06-05 17:04:48 -0400

    Functions for new endpoints (Angler Questions/Answers, SC 012 Lake, Stream Dimensions. New function to change management unit from slug to name.

---
+++ R/get_Angler_Answers.R
@@ -0,0 +1,50 @@
+#' Get Angler Answers from Creel Portal API
+#'
+#' This function accesses the api endpoint for Angler Answers. Angler 
+#' Answer records contain the multiple choice answers to any extra 
+#' questions that were asked during creels. Each answer has two parts: the
+#' Answer_Number and Answer_Text. The Answer_Number is recorded in the FN121
+#' table in the ANGOP# fields, where the digit in the ANGOP# field corresponds
+#' with the Question_Number. The Answer_Text is the text that corresponds
+#' with the Answer_Number. This function takes an optional filter list which 
+#' can be used to return records based on the answer number, text, and
+#' several attributes of the project such as project code, 
+#' or part of the project code, lake, first year, last year, etc.
+#' 
+#' Use ~show_filters("angler_answers")~ to see the full list of available filter
+#' keys (query parameters)
+#'
+#' @param filter_list list
+#' @param show_id When 'FALSE', the default, the 'id' and 'slug'
+#' fields are hidden from the data frame. To return these columns
+#' as part of the data frame, use 'show_id = TRUE'.
+#'
+#' @param to_upper - should the names of the dataframe be converted to
+#' upper case?
+#'
+#' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+#' @return dataframe
+#' @export
+#' @examples
+#'
+#' angler_answers <- get_Angler_Answers(list(lake = "ON", year__gte = 2012, year__lte = 2018))
+#'
+#' angler_answers <- get_Angler_Answers(list(lake = "ER"))
+#'
+#' angler_answers <- get_Angler_Answers(list(lake = "HU", prj_cd__like = "_001"))
+#'
+#' angler_answers <- get_Angler_Answers(list(lake = "HU", protocol = "USA"), show_id = TRUE)
+get_Angler_Answers <- function(filter_list = list(), show_id = FALSE, to_upper = TRUE) {
+  recursive <- ifelse(length(filter_list) == 0, FALSE, TRUE)
+  query_string <- build_query_string(filter_list)
+  check_filters("angler_answers", filter_list, api_app = "creels")
+  my_url <- sprintf(
+    "%s/angler_answers/%s",
+    get_sc_portal_root(),
+    query_string
+  )
+  payload <- api_to_dataframe(my_url, recursive = recursive)
+  payload <- prepare_payload(payload, show_id, to_upper)
+
+  return(payload)
+}
---
+++ R/get_Stream_Dimensions.R
@@ -0,0 +1,48 @@
+#' Get Stream Dimensions data from FN_Portal API
+#'
+#' This function accesses the api endpoint for Stream_Dimensions
+#' records. Stream_Dimensions records contain information about 
+#' streams and rivers in which electrofishing transects take place,
+#' including the distance from the start of the transect, the distance 
+#' across the channel, and stream depth, width, and velocity. Other 
+#' relevant details for the SUBSPACE are found in the FN121 table. 
+#' This function takes an optional filter list which can be used to 
+#' return records based on attributes of the project including project
+#' code, or part of the project code, lake, first year, last year,
+#' protocol, etc.
+#'
+#' See
+#' http://10.167.37.157/fn_portal/api/v1/redoc/#operation/stream_dimensions_list
+#' for the full list of available filter keys (query parameters)
+#'
+#' @param filter_list list
+#' @param show_id When 'FALSE', the default, the 'slug'
+#' field is hidden from the data frame. To return this field
+#' as part of the data frame, use 'show_id = TRUE'.
+#' @param to_upper - should the names of the dataframe be converted to
+#' upper case?
+#'
+#' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+#' @return dataframe
+#' @export
+#' @examples
+#'
+#' show_filters("stream_dimensions")
+#'
+#' stream <- get_Stream_Dimensions(list())
+#' 
+#' stream <- get_Stream_Dimensions(list())
+get_Stream_Dimensions <- function(filter_list = list(), show_id = FALSE, to_upper = TRUE) {
+  recursive <- ifelse(length(filter_list) == 0, FALSE, TRUE)
+  query_string <- build_query_string(filter_list)
+  check_filters("stream_dimensions", filter_list, "fn_portal")
+  my_url <- sprintf(
+    "%s/stream_dimensions/%s",
+    get_fn_portal_root(),
+    query_string
+  )
+  payload <- api_to_dataframe(my_url, recursive = recursive)
+  payload <- prepare_payload(payload, show_id, to_upper)
+
+  return(payload)
+}
