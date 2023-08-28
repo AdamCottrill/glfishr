5445656dc300f4ed31548bb37f07d3efc6b914ca
Rachel Henderson <rhenderson1993@gmail.com>
2023-08-28 15:57:48 -0400

    Added function to get abstracts from Project Tracker. No functionality to download images yet, but the link is provided.

---
+++ R/get_pt_abstracts.R
@@ -0,0 +1,47 @@
+#' get_pt_abstracts - Project Tracker Abstracts
+#'
+#' This function accesses the api endpoint for abstracts that have
+#' been uploaded to project tracker. This api endpoint
+#' accepts a large number of filters associated with the project or
+#' report type. Project specific filters include project code(s),
+#' years, lakes, and project lead. Use 'show_filters("project_abstracts")' 
+#' to see the full list of available filters.
+#'
+#'
+#' @param filter_list list
+#'
+#' @param to_upper - should the names of the returned dataframe be
+#'   converted to upper case?
+#'
+#'
+#' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+#' @return dataframe
+#' @export
+#' @examples
+#'
+#' abstracts <- get_pt_abstracts(list(
+#'   lake = "ON", year__gte = 2012,
+#'   year__lte = 2018
+#' ))
+#' 
+#' abstracts <- get_pt_abstracts(list(lake = "ER"))
+#'
+#' filters <- list(lake = "SU", prj_cd = c("LSA_IA15_CIN", "LSA_IA17_CIN"))
+#' abstracts <- get_pt_abstracts(filters)
+#'
+#' abstracts <- get_pt_abstracts(list(lake = "HU"))
+get_pt_abstracts <- function(filter_list = list(), to_upper = TRUE) {
+  recursive <- ifelse(length(filter_list) == 0, FALSE, TRUE)
+  query_string <- build_query_string(filter_list)
+  check_filters("project_abstracts", filter_list, api_app = "project_tracker")
+  my_url <- sprintf(
+    "%s/project_abstracts/%s",
+    get_pt_portal_root(),
+    query_string
+  )
+  
+  payload <- api_to_dataframe(my_url, recursive = recursive)
+  payload <- prepare_payload(payload, to_upper = to_upper)
+  
+  return(payload)
+}
