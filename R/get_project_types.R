a959f4e492eadac7143415aff33d06ac1e46d21c
ArthurBonsall <arthur.bonsall@ontario.ca>
2023-10-13 14:29:04 -0400

    Added the functions get_fisheries_offices, get_project_types, and get_tag_colours.

---
+++ R/get_project_types.R
@@ -0,0 +1,42 @@
+#' Get project type options - A list of project type codes used in GLIS
+#'
+#' This function accesses the api endpoint for project type choices and returns
+#' their labels, descriptions and whether they're in use. It fetches
+#' the entire table of accepted project types - no other filter
+#' parameters are currently available for this endpoint. Project type codes
+#' represent the fifth and sixth digits of project code (PRJ_CD).
+#'
+#'
+#' See
+#' https://intra.glis.mnr.gov.on.ca/common/project_types/
+#' for the full list of fisheries_office options
+#'
+#'
+#' @param filter_list list
+#'
+#' @param to_upper - should the names of the dataframe be converted to
+#' upper case?
+#'
+#' @author Rachel Henderson \email{rachel.henderson@@ontario.ca}
+#' @return dataframe
+#' @export
+#' @examples
+#'
+#' fisheries_offices <- get_fisheries_offices()
+#' fisheries_office_slugs <- get_fisheries_offices(show_id = TRUE)
+get_project_types <- function(filter_list = list(), show_id = FALSE, to_upper = TRUE) {
+  query_string <- build_query_string(filter_list)
+  common_api_url <- get_common_portal_root()
+  # check_filters("project_types", filter_list, "common")
+  # TODO: add a warning about 'all=TRUE' being the only allowed filter
+  
+  my_url <- sprintf(
+    "%s/project_types/%s",
+    common_api_url,
+    query_string
+  )
+  payload <- api_to_dataframe(my_url)
+  payload <- prepare_payload(payload, show_id, to_upper)
+  
+  return(payload)
+}
