501d5193ca74be51c3cb40f6f4b0f06233f2ec93
Cottrill <adam.cottrill@ontario.ca>
2023-12-05 15:00:09 -0500

    Re-factored get_api_filters and added associated tests. Updated docs.
    This commit re-factors the get_api_filters functions into smaller more
    focused functions that can be (more) easily tested. Tests were added
    to get_swagger_url, parse_swagger_payload, add_special_filters, and
    one that verifies an error is raised if the url cannot be found. All
    associated documentation was re-generated. All other tests continue to
    pass.

--- R/get_api_filters.R
+++ R/get_api_filters.R
@@ -18,20 +18,11 @@
 #' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
 #' @return list
 get_api_filters <- function(api_app, create_list = TRUE) {
-  domain <- get_domain()
-  swagger_url <- sprintf("%s%s/api/v1/swagger.json", domain, api_app)
-
-
-  # need to run GET(swagger_url) twice until Azure login is working
-  redundant_response <- tryCatch(httr::GET(swagger_url),
-    error = function(err) {
-      print("unable to fetch filters from the server. Is your VPN active?")
-    }
-  )
+  swagger_url <- get_swagger_url(api_app)
 
   response <- tryCatch(httr::GET(swagger_url),
     error = function(err) {
-      print("unable to fetch filters from the server. Is your VPN active?")
+      stop("unable to fetch filters from the server. Is your VPN active?")
     }
   )
 
@@ -46,46 +37,110 @@ get_api_filters <- function(api_app, create_list = TRUE) {
     }
   )
 
-  api_filters <- list()
-  for (name in names(payload$paths)) {
-    endpoint <- gsub("/", "", name)
-    parameters <- payload$paths[[name]]$get$parameters
-    if (length(parameters) & !grepl("\\{", endpoint)) {
-      values <- subset(parameters, select = c("name", "description"))
+  api_filters <- parse_swagger_payload(payload)
 
-      if (name == "/fn121/") {
-        values <- rbind(values, data.frame(name = "mu_type", description = "Run get_mu_list() for a list of accepted abbreviations."))
-      }
-
-      if (name == "/gear/") {
-        values <- rbind(values, data.frame(
-          name = c("all", "depreciated", "confirmed"),
-          description = c("Boolean value.", "Boolean value.", "Boolean value.")
-        ))
-      }
-
-      if (name == "/fn012_protocol/") {
-        values <- rbind(values, data.frame(
-          name = c("all", "active", "confirmed"),
-          description = c("Boolean value.", "Boolean value.", "Boolean value.")
-        ))
-      }
-
-      if (name == "/prj_ldr/") {
-        values <- rbind(values, data.frame(name = "all", description = "Boolean value; default is FALSE."))
-      }
-
-      if (name == "/species/") {
-        values <- rbind(values, data.frame(name = "detail", description = "Boolean value; default is FALSE."))
-      }
-
-      # api_filters <- c(api_filters, parse(endpoint) = values)
-      api_filters[[endpoint]] <- values
-    }
-  }
   if (create_list == TRUE) {
     assign("api_filters", api_filters, envir = .GlobalEnv)
   } else {
     api_filters
   }
+}
+
+
+##' Build the url to fetch teh swagger data for a give app
+##'
+##' An internal  helper function that combines the applicaiton name
+##' with the current domain to build the url for the swagger
+##' documentation for that app.
+##' @param api_app - the name of the api application to fetch the
+##' filters infromation from.
+##' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+##' @return character vector
+get_swagger_url <- function(api_app = c(
+                              "common", "project_tracker",
+                              "fn_portal", "creels", "tfat"
+                            )) {
+  api_app <- match.arg(api_app)
+  domain <- get_domain()
+  swagger_url <- sprintf("%s%s/api/v1/swagger.json", domain, api_app)
+  return(swagger_url)
+}
+
+##' Convert swagger response into named list
+##'
+##' the response from the swagger enpoint include a large number of
+##' attributes and details that are not need for glfishr. this
+##' function takes the named list created from the json response and
+##' extracts the name and description of each GET parameter that can
+##' be used as filters.
+##'
+##' Given the complexity of the payload, this function is not
+##' currently tested
+##'
+##' @param payload
+##'
+##' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+##' @return  list
+parse_swagger_payload <- function(payload) {
+  api_filters <- list()
+  for (name in names(payload$paths)) {
+    endpoint_name <- gsub("/", "", name)
+    parameters <- payload$paths[[name]]$get$parameters
+    if (length(parameters) && !grepl("\\{", endpoint_name)) {
+      values <- parameters[, c("name", "description")]
+      values <- add_special_filters(endpoint_name, values)
+      # api_filters <- c(api_filters, parse(endpoint) = values)
+      api_filters[[endpoint_name]] <- values
+    }
+  }
+  return(api_filters)
+}
+
+
+##' Add special fitler options to seelected endpoints
+##'
+##' Some of the the api endpoints accept filter arguments that are not
+##' captured in the automatically generated swagger docus.  These need
+##' to be added so they can be presented to glfishr users.
+##' @param endpoint_name - the name of api endpoint
+##' @param values - dataframe with existing filter attributes
+##' @author Adam Cottrill \email{adam.cottrill@@ontario.ca}
+##' @return list
+add_special_filters <- function(endpoint_name, values) {
+  if (endpoint_name == "fn121") {
+    values <- rbind(values, data.frame(
+      name = "mu_type",
+      description = "Run get_mu_list() for a list of accepted abbreviations."
+    ))
+  }
+
+  if (endpoint_name == "gear") {
+    values <- rbind(values, data.frame(
+      name = c("all", "depreciated", "confirmed"),
+      description = c("Boolean value.", "Boolean value.", "Boolean value.")
+    ))
+  }
+
+  if (endpoint_name == "fn012_protocol") {
+    values <- rbind(values, data.frame(
+      name = c("all", "active", "confirmed"),
+      description = c("Boolean value.", "Boolean value.", "Boolean value.")
+    ))
+  }
+
+  if (endpoint_name == "prj_ldr") {
+    values <- rbind(values, data.frame(
+      name = "all",
+      description = "Boolean value; default is FALSE."
+    ))
+  }
+
+  if (endpoint_name == "species") {
+    values <- rbind(values, data.frame(
+      name = "detail",
+      description = "Boolean value; default is FALSE."
+    ))
+  }
+
+  return(values)
 }
---
+++ tests/testthat/test-add_special_filters.R
@@ -0,0 +1,53 @@
+##' the add_special_filters special fitler options to seelected endpoints
+##'
+##' Some of the the api endpoints accept filter arguments that are not
+##' captured in the automatically generated swagger docus.  These need
+##' to be added so they can be presented to glfishr users.
+##'
+##' If new filters are added in the future, test cases should be add
+##' here too.
+
+
+filters <- data.frame(name = character(), description = character())
+
+
+test_that("unknown endpoint name has no effect ", {
+  observed <- add_special_filters("foo", filters)
+  expect_equal(observed, filters)
+})
+
+
+test_that("fn121 has filters added for 'mu_type'", {
+  observed <- add_special_filters("fn121", filters)
+  expect_equal(nrow(observed), 1)
+  expect_equal(observed$name, c("mu_type"))
+})
+
+
+test_that("gear has filters added for 'all', 'depreciated', confirmed", {
+  observed <- add_special_filters("gear", filters)
+  expect_equal(nrow(observed), 3)
+  expect_equal(observed$name, c("all", "depreciated", "confirmed"))
+})
+
+
+test_that("fn012_protocol has filters added for 'all', 'active', confirmed", {
+  observed <- add_special_filters("fn012_protocol", filters)
+  expect_equal(nrow(observed), 3)
+  expect_equal(observed$name, c("all", "active", "confirmed"))
+})
+
+
+test_that("prj_ldr has filter added for 'all'", {
+
+  observed <- add_special_filters("prj_ldr", filters)
+  expect_equal(nrow(observed), 1)
+  expect_equal(observed$name, c("all"))
+})
+
+
+test_that("species has filter added for 'detail'", {
+  observed <- add_special_filters("species", filters)
+  expect_equal(nrow(observed), 1)
+  expect_equal(observed$name, c("detail"))
+})
