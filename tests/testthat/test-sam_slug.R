1b9fb9c54e3e228fef425b9eeaeff19b8ab70a23
Cottrill <adam.cottrill@ontario.ca>
2024-04-02 11:22:40 -0400

    Missing tests and utility functions

---
+++ tests/testthat/test-sam_slug.R
@@ -0,0 +1,19 @@
+test_that("fn122 slug is trimmed to fn121", {
+  slug_in = "lha_ia10_006-111-1"
+  slug_out = "lha_ia10_006-111"
+  expect_equal(sam_slug(slug_in), slug_out)
+})
+
+
+test_that("fn126 slug is trimmed to fn125", {
+  slug_in = "lha_ia10_006-111-1-091-00-1-6"
+  slug_out = "lha_ia10_006-111"
+  expect_equal(sam_slug(slug_in), slug_out)
+})
+
+
+test_that("fn125 slug can trimmed to fn123 with levels", {
+  slug_in = "lha_ia10_006-111-1-091-00-1"
+  slug_out = "lha_ia10_006-111"
+  expect_equal(sam_slug(slug_in), slug_out)
+})
